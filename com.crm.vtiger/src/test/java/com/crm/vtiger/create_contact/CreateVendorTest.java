548805c6c47d5aef3bdf7fca456f4706d4fe8d8f
Ramya <Ramya@LAPTOP-V2H6MTKJ>
2022-03-25 10:33:31 +0530

    Ramya created vendor test class

---
+++ com.crm.vtiger/src/test/java/com/crm/vtiger/create_contact/CreateVendorTest.java
@@ -0,0 +1,12 @@
+package com.crm.vtiger.create_contact;
+
+import org.testng.annotations.Test;
+
+public class CreateVendorTest {
+
+	@Test
+	public void createVendor()
+	{
+		System.out.println("created vendor successfully");
+	}
+}
